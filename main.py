3687db99b857489a8bf9253fcf002b0ed967d907
Raiid Ahmed <raiidahmed@gmail.com>
2023-05-28 16:08:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import openai
-openai.api_key = "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"  # supply your API key however you choose
+openai.api_key = ""  # supply your API key however you choose
 
 image_resp = openai.Image.create(prompt="Raiid Ahmed", n=4, size="512x512")
 
